3e3ceb863c7eb93786339dd7a124b14454054c83
mohammedsaif001 <mdsaif4online@gmail.com>
2022-12-13 16:09:41 +0530

    Home Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Header from './components/Header';
-
+import Header from './components/header/Header';
+import Home from './components/home/Home';
 function App() {
   return (
     <>
       <Header />
+      <main className="main">
+        <Home />
+      </main>
     </>
   );
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Social() {
+    return (
+        <div className='home__social'>
+
+            <a href="https://www.instagram.com/mohammedsaif001/" className="home__social-icon" target="_blank">
+                <i className="uil uil-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/mohammedsaif001/" className="home__social-icon" target="_blank">
+                <i className="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="https://github.com/mohammedsaif001" className="home__social-icon" target="_blank">
+                <i className="uil uil-github-alt"></i>
+            </a>
+
+        </div>
+    )
+}
+
+export default Social
