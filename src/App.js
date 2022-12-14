2e76541903e89f4516c2273818cfef892aa29b84
mohammedsaif-nviera <mohammedsaif@nviera.com>
2022-12-14 18:45:27 +0530

    Scroll Up section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
+import ScrollUp from './components/scrollup/ScrollUp';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -16,8 +17,9 @@ function App() {
         <Skills />
         <Qualification />
         <Contact />
-        {/* <Footer /> */}
       </main>
+      {/* <Footer /> */}
+      <ScrollUp />
     </>
   );
 }
