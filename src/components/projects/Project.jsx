47915c45fb830d850c70e4033d7a702f68eceb0b
mohammedsaif-nviera <mohammedsaif@nviera.com>
2022-12-15 17:39:22 +0530

    Services to projects name change

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -15,49 +15,49 @@ const Project = () => {
     isToggle ? document.body.style.overflow = 'hidden' : document.body.style.removeProperty('overflow');
 
     return (
-        <section className="services section" id="services">
+        <section className="projects section" id="projects">
             <h2 className="section__title">Projects</h2>
             <span className="section__subtitle">Work Experience & Personal Projects</span>
 
-            <div className="services__container container grid">
-                <div className="services__content ">
+            <div className="projects__container container grid">
+                <div className="projects__content ">
                     <div>
-                        <i className="uil uil-web-grid services__icon">
+                        <i className="uil uil-web-grid projects__icon">
                         </i>
-                        <h3 className="services__title">Nviera Project</h3>
+                        <h3 className="projects__title">Nviera Project</h3>
                     </div>
-                    <span className="services__button" onClick={() => toggleTab(1)}>View More
-                        <i className="uil uil-arrow-right services__button-icon"></i>
+                    <span className="projects__button" onClick={() => toggleTab(1)}>View More
+                        <i className="uil uil-arrow-right projects__button-icon"></i>
                     </span>
-                    <div className={toggleState === 1 ? "services__modal active__modal" : "services__modal"}>
-                        <div className='services__modal-content '>
-                            <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-                            <h3 className="services__modal-title">Nviera Project</h3>
-                            <p className="services__modal-description">
+                    <div className={toggleState === 1 ? "projects__modal active__modal" : "projects__modal"}>
+                        <div className='projects__modal-content '>
+                            <i className="uil uil-times projects__modal-close" onClick={() => toggleTab(0)}></i>
+                            <h3 className="projects__modal-title">Nviera Project</h3>
+                            <p className="projects__modal-description">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorem officiis molestias. Recusandae deleniti enim magni illo aliquam corrupti provident.
                             </p>
-                            <ul className="services__modal-services grid">
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                            <ul className="projects__modal-projects grid">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
@@ -65,44 +65,44 @@ const Project = () => {
                         </div>
                     </div>
                 </div>
-                <div className="services__content">
+                <div className="projects__content">
                     <div>
-                        <i className="uil uil-arrow services__icon">
+                        <i className="uil uil-arrow projects__icon">
                         </i>
-                        <h3 className="services__title">Medical Image <br />Denoising</h3>
+                        <h3 className="projects__title">Medical Image <br />Denoising</h3>
                     </div>
-                    <span className="services__button" onClick={() => toggleTab(2)}>View More
-                        <i className="uil uil-arrow-right services__button-icon" ></i>
+                    <span className="projects__button" onClick={() => toggleTab(2)}>View More
+                        <i className="uil uil-arrow-right projects__button-icon" ></i>
                     </span>
-                    <div className={toggleState === 2 ? "services__modal active__modal" : "services__modal"}>
-                        <div className='services__modal-content '>
-                            <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-                            <h3 className="services__modal-title">Medical Image Denoising</h3>
-                            <p className="services__modal-description">
+                    <div className={toggleState === 2 ? "projects__modal active__modal" : "projects__modal"}>
+                        <div className='projects__modal-content '>
+                            <i className="uil uil-times projects__modal-close" onClick={() => toggleTab(0)}></i>
+                            <h3 className="projects__modal-title">Medical Image Denoising</h3>
+                            <p className="projects__modal-description">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorem officiis molestias. Recusandae deleniti enim magni illo aliquam corrupti provident.
                             </p>
-                            <ul className="services__modal-services grid">
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                            <ul className="projects__modal-projects grid">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
@@ -110,44 +110,44 @@ const Project = () => {
                         </div>
                     </div>
                 </div>
-                <div className="services__content">
+                <div className="projects__content">
                     <div>
-                        <i className="uil uil-edit services__icon">
+                        <i className="uil uil-edit projects__icon">
                         </i>
-                        <h3 className="services__title">News <br /> Flash</h3>
+                        <h3 className="projects__title">News <br /> Flash</h3>
                     </div>
-                    <span className="services__button" onClick={() => toggleTab(3)}>View More
-                        <i className="uil uil-arrow-right services__button-icon"></i>
+                    <span className="projects__button" onClick={() => toggleTab(3)}>View More
+                        <i className="uil uil-arrow-right projects__button-icon"></i>
                     </span>
-                    <div className={toggleState === 3 ? "services__modal active__modal" : "services__modal"}>
-                        <div className='services__modal-content '>
-                            <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)}></i>
-                            <h3 className="services__modal-title">News Flash</h3>
-                            <p className="services__modal-description">
+                    <div className={toggleState === 3 ? "projects__modal active__modal" : "projects__modal"}>
+                        <div className='projects__modal-content '>
+                            <i className="uil uil-times projects__modal-close" onClick={() => toggleTab(0)}></i>
+                            <h3 className="projects__modal-title">News Flash</h3>
+                            <p className="projects__modal-description">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorem officiis molestias. Recusandae deleniti enim magni illo aliquam corrupti provident.
                             </p>
-                            <ul className="services__modal-services grid">
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                            <ul className="projects__modal-projects grid">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
-                                <li className="services__modal-service">
-                                    <i className="uil uil-check-circle services__modal-icon"></i>
-                                    <p className="services__modal-info">
+                                <li className="projects__modal-project">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
                                         Cretae Lorem, ipsum dolor.
                                     </p>
                                 </li>
