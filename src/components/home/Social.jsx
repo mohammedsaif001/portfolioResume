177870c355e3f2b854f7a937d1477e0bd50de601
Mohammed Saif <mdsaif4online@gmail.com>
2023-08-04 18:06:06 +0530

    Replacing social Icons

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,8 +4,8 @@ function Social() {
     return (
         <div className='home__social'>
 
-            <a href="https://www.instagram.com/mohammedsaif001/" className="home__social-icon" target="_blank">
-                <i className="uil uil-instagram"></i>
+            <a href="mailto:mohammed001saif@gmail.com" className="home__social-icon" target="_blank">
+                <i className="bx bx-mail-send"></i>
             </a>
             <a href="https://www.linkedin.com/in/mohammedsaif001/" className="home__social-icon" target="_blank">
                 <i className="uil uil-linkedin-alt"></i>
